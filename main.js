e98947b69ffa83955c10f078e39fd212fd3cfb70
Neguete10 <hmedeirosdelima@cedarville.edu>
2024-05-16 23:40:52 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow } = require('electron');
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+  });
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({        
+        width: 1024,
+        height: 600,
+        show: false,
+    });
+
+    win.loadFile('home.html');
+
+    win.once('ready-to-show', () => {
+        win.minimize();
+    });
+
+    win.setMenu(null);
+
+    // Handle window close
+    win.on('closed', () => {
+        // Dereference the window object
+        win = null;
+    });
+}
+
+// When Electron has finished initialization, create the window
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('before-quit', () => {
+    if (win && !win.isDestroyed()) {
+        if (win.webContents.isDevToolsOpened()) {
+            win.webContents.closeDevTools();
+        }
+    }
+});
+// On macOS, re-create the window when the dock icon is clicked and no other windows are open
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
