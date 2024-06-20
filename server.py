3879e94ef90d1644d83d5c779cae9df769d634ea
Neguete10 <hmedeirosdelima@cedarville.edu>
2024-06-20 00:52:51 -0400

    refactor: Update pump control endpoint

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
-import json
 
 app = Flask(__name__)
 
-@app.route('/activate_pump', methods=['POST'])
-def activate_pump():
-    print('Received request to activate pump' + json.dumps(request.get_json()))
-    return json.dumps({'message': 'Pump activated'})
+@app.route('/activate_pumps', methods=['GET', 'POST'])
+def print_request():
+    print('Headers: ', request.headers)
+    print('Body: ', request.get_data())
+    return 'Request printed in console'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
